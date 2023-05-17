689ec9adc64781c4e105e3654e039eed5fc19dbd
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-05-17 17:20:00 +0530

    payable

---
+++ payble.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity  ^0.8.17;
+ contract Payble{
+    address payable user=payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+      function payether() public payable{
+             
+      }
+      function getbalance() public view returns(uint){
+           return address(this).balance;
+      }
+      function sendether() public {
+           user.transfer(1 ether);
+      }
+
+ }
