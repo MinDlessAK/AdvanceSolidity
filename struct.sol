86d6fd4d7d931e17130d647ecb93bfb350fe6b10
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-05-17 17:21:12 +0530

    structure

---
+++ struct.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity  ^0.8.17;
+struct  Student{
+  uint  roll;
+  string name;
+
+}
+contract demo{
+    Student public s1;
+    constructor(uint _roll,string memory _name){
+         s1.roll=_roll;
+         s1.name=_name;
+    }
+     function change(uint _roll,string memory _name)public{
+         Student memory new_student=Student({
+            roll:_roll,
+            name:_name
+         });
+         s1=new_student;
+    }
+
+}
