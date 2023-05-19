f4e201470fb9272298bafccbb2fb0df662da8e96
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-05-19 11:54:23 +0530

    functionSelector

--- FunctionSelector.sol
+++ FunctionSelector.sol
@@ -1,9 +1,17 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.17;
+contract test{
+    function TestSelector(string calldata _data)external returns(bytes4) {
+        return bytes4(keccak256(bytes(_data)));
+
+    }
+}
+
 
 contract functionSelector{
-    function Hillo() external{
-          
+    event Selector(bytes data);
+    function getSelector(uint _amount) external{
+       emit Selector(msg.data);  
     }
 
 }
