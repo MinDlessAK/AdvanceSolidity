2234ec3c5a237608b1f7b408eb4940470c22356d
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-05-19 22:56:48 +0530

    AbiDecode

--- abiDecode.sol
+++ abiDecode.sol
@@ -1,5 +1,38 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.17;
+
+// ABI Decode
+// abi.encode encodes data into bytes.
+
+// abi.decode decodes bytes back into data.
  contract AbiDecode{
-    
+ 
+struct Mystruct{
+     string name;
+     uint[2] nums;
+} 
+ 
+ 
+    function encode(
+        uint x,
+        address addr,
+        uint[] memory arr,
+        Mystruct memory myStruct
+        //dont know it use calldata
+        // uint[] calldata arr,
+        // Mystruct calldata myStruct 
+        )
+        external pure returns(bytes memory){
+            return abi.encode(x,addr,arr,myStruct);
+    }
+    function decode(bytes calldata data)
+    external pure 
+    returns(
+        uint x,
+        address addr,
+        uint[]  memory arr,
+        Mystruct memory myStruct
+    ){
+      (x,addr,arr,myStruct)=abi.decode(data,(uint,address,uint[],Mystruct));
+    }
  }
