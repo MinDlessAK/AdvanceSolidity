a9c96592c5243d1a49c9e2fdbcdc401a4437e13f
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-05-25 10:57:54 +0530

    hashing

---
+++ HashingWithKecaak.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.17;
+//hashing with keccak256
+  //keccak256 compute the keccak-256 hadsh of input
+   //Creating a determinitic unique id from a input 
+   //commit-reveal scheme
+   //compact cryptographic signature (by signing the hash instead of larger input)
+    
